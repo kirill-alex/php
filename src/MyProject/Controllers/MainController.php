ee5cc70c3c38a3ff785d6d8f5d5618a5022294d3
Alexeev Kirill <kirill.alexee@gmail.com>
2026-05-18 21:23:06 +0300

    lab8_1

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -25,7 +25,10 @@ class MainController
 
     public function sayHello(string $name): void
     {
-        $this->view->renderHtml('main/hello.php', ['name' => $name]);
+        $this->view->renderHtml('main/hello.php', [
+            'name'  => $name,
+            'title' => 'Страница приветствия',
+        ]);
     }
 
     public function sayBye(string $name): void
--- templates/header.php
+++ templates/header.php
@@ -2,8 +2,8 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Мой блог</title>
-    <link rel="stylesheet" href="/php/styles/styles.css">
+    <title><?= $title ?? 'Мой блог' ?></title>
+    <link rel="stylesheet" href="/styles/style.css">
 </head>
 <body>
 
